4a64da238bbf580a1c5752bd4b8f10917b88f752
huangcheng <921365631@qq.com>
2019-01-18 12:00:39 +0800

    shangci

--- serve/index.js
+++ serve/index.js
@@ -25,7 +25,7 @@ app.get('/homeport',function(req,res){
 
 
 app.get('/port',function(req,res){
-	mock.saveIp(req.ip.replace('::ffff:',''),req.query.port)
+	mock.saveIp(req.query.ip,req.query.port)
 	res.end();
 	// res.sendFile(path.resolve(__dirname,'../dist/index.html'))
 })
